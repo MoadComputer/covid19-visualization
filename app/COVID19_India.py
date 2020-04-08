6f2e278ad26dbb8b994c1a7f62bfc91a4affaa13
Rahul Remanan <rahul@remanan.net>
2020-04-08 17:51:14 +0530

    Added Map tile overlay support

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -10,15 +10,14 @@ from bokeh.io.doc import curdoc
 from bokeh.palettes import brewer
 from bokeh.plotting import figure
 from bokeh.plotting import show as plt_show
+from bokeh.tile_providers import Vendors, get_provider
 from bokeh.io import output_notebook, show, output_file
 from bokeh.layouts import widgetbox, row, column, gridplot
-from bokeh.tile_providers import Vendors, get_provider
-from bokeh.models import Slider, HoverTool, Select, Div, Range1d, WMTSTileSource
 from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, NumeralTickFormatter
+from bokeh.models import Slider, HoverTool, Select, Div, Range1d, WMTSTileSource, BoxZoomTool
 
 India_statewise = geopandas.read_file('https://github.com/MoadComputer/covid19-visualization/raw/master/data/GeoJSON_assets/India_statewise_minified.geojson')
-India_statewise.crs = {'init': 'epsg:4326'}
-India_statewise.head()
+India_statewise = India_statewise.to_crs("EPSG:3395")
 
 covid19_data = pd.read_csv('https://github.com/MoadComputer/covid19-visualization/raw/master/data/India_statewise/COVID19_India_statewise.csv')
 covid19_data.loc[covid19_data['state'] == 'Telengana', 'state'] = 'Telangana'
@@ -42,13 +41,15 @@ merged_json = merged_data['json_data']
 def covid19_plot(covid19_geosource, 
                  input_df=None,
                  input_field=None,
-                 plot_title=None):
+                 plot_title=None,
+                 map_overlay=True,
+                 enable_toolbar=True):
   palette = brewer['Oranges'][8]
   palette = palette[::-1]
   hover = HoverTool(tooltips = [('State','@state'),
-                                ('Cases', '@total_cases'),
-                                #('Discharged', '@discharged'),
-                                ('Deaths', '@deaths')])
+                                ('Total cases', '@total_cases'),
+                                ('Total discharged/migrated', '@discharged'),
+                                ('Total deaths', '@deaths')])
   color_mapper = LinearColorMapper(palette = palette, 
                                    low = 0, 
                                    high = int(10*(np.ceil(np.max(input_df[input_field].values)/10))))
@@ -58,24 +59,36 @@ def covid19_plot(covid19_geosource,
                        formatter=format_tick,
                        border_line_color=None, 
                        location = (0, 0))
+  if map_overlay:
+    xmin = 8450000
+    xmax = 10000000
+    ymin = 850000
+    ymax = 4550000
   plt = figure(title = plot_title,
-               tools='', 
-               plot_height = 550, plot_width = 640,
-               toolbar_location = None, #'left',
-               lod_factor=int(1e7),
+               x_range=(xmin, xmax) if map_overlay else None,
+               y_range=(ymin, ymax) if map_overlay else None,
+               tools='save' if enable_toolbar else '', 
+               plot_height = 640, plot_width = 640,
+               toolbar_location = 'left' if enable_toolbar else None,
+               #lod_factor=int(1e7),
                #lod_threshold=int(2),
                output_backend="webgl"
-             )
+               )
+  if map_overlay:
+    wmts = WMTSTileSource(url="http://c.tile.openstreetmap.org/{Z}/{X}/{Y}.png")
+    plt.add_tile(wmts)
+    plt.xaxis.axis_label = 'longitude'
+    plt.yaxis.axis_label = 'latitude'
   plt.xgrid.grid_line_color = None
   plt.ygrid.grid_line_color = None
   plt.axis.visible = False
   plt.patches('xs','ys', 
-            source = covid19_geosource, 
-            fill_color = {'field' : 'deaths', 
-                          'transform' : color_mapper},
-            line_color = 'red', 
-            line_width = 1.25, 
-            fill_alpha = 1)
+              source = covid19_geosource, 
+              fill_color = {'field' : 'deaths', 
+                            'transform' : color_mapper},
+              line_color = 'red', 
+              line_width = 0.34, 
+              fill_alpha = 0.85)
   plt.add_layout(color_bar, 'right')
   plt.add_tools(hover)
   plt.title.text_font_size = '34pt'
