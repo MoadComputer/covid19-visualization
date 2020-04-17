96b235efe8c7ca5781cbf4d6067c3a603b887536
Rahul Remanan <rahul@remanan.net>
2020-04-17 13:43:07 +0530

    Enabled advanced plotting with COVID19 India forecasting model

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -8,13 +8,14 @@ import pandas as pd
 
 from bokeh.io.doc import curdoc
 from bokeh.plotting import figure
+from bokeh.models.widgets import Button
 from bokeh.palettes import brewer, OrRd
 from bokeh.plotting import show as plt_show
 from bokeh.tile_providers import Vendors, get_provider
 from bokeh.io import output_notebook, show, output_file
 from bokeh.layouts import widgetbox, row, column, gridplot
 from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, NumeralTickFormatter
-from bokeh.models import Slider, HoverTool, Select, Div, Range1d, WMTSTileSource, BoxZoomTool
+from bokeh.models import Slider, HoverTool, Select, Div, Range1d, WMTSTileSource, BoxZoomTool, TapTool, Panel, Tabs
 
 verbose=False
 
@@ -51,6 +52,83 @@ merged_data = covid19_json(covid19_data, India_statewise,
                            verbose=verbose)
 merged_json = merged_data['json_data']
 
+def CustomPalette(palette_type):
+  if palette_type.lower()=='OrRd'.lower():
+    palette = OrRd[9]
+    palette = palette[::-1]
+  else:
+    palette = brewer['Oranges']
+    palette = palette[::-1]
+  return palette
+
+def CustomHoverTool(enable_advancedStats, custom_hovertool):
+  advancedStats_hover=HoverTool(tooltips ="""<strong><font face="Arial" size="3">@state</font></strong> <br>
+                                             <font face="Arial" size="3">Cases: @total_cases</font><br>
+                                             <font face="Arial" size="3">Deaths: @deaths </font>
+                                             <hr>
+                                             <font face="Arial" size="3">Forecast (+1 day cases): @preds_cases </font>""")
+
+  simpleStats_hover=HoverTool(tooltips ="""<strong><font face="Arial" size="3">@state</font></strong> <br>
+                                           <font face="Arial" size="3">Cases: @total_cases</font><br>
+                                           <font face="Arial" size="3">Deaths: @deaths </font>""")
+
+  standard_hover = HoverTool(tooltips = [('State','@state'),
+                                         ('Cases', '@total_cases'),
+                                         #('Discharged/migrated', '@discharged'),
+                                         ('Deaths', '@deaths')])
+  
+  if enable_advancedStats:
+    hover = advancedStats_hover
+  elif custom_hovertool:
+    hover  = simpleStats_hover
+  else:
+    hover = standard_hover
+  return hover
+
+def MapOverlayFormatter(map_overlay):
+  if map_overlay:
+    xmin = 8450000
+    xmax = 10000000
+    ymin = 850000
+    ymax = 4550000
+    
+    return xmin, xmax, ymin, ymax
+
+def geographic_overlay(plt, 
+                       geosourceJson=None,
+                       colorBar=None,
+                       colorMapper=None,
+                       colorMode='',
+                       hoverTool=None,
+                       mapOverlay=True,
+                       enableTapTool=False,
+                       enableToolbar=True):
+  if mapOverlay:
+    wmts = WMTSTileSource(url="http://c.tile.openstreetmap.org/{Z}/{X}/{Y}.png")
+    plt.add_tile(wmts)
+    plt.xaxis.axis_label = 'longitude'
+    plt.yaxis.axis_label = 'latitude'
+  
+  plt.xgrid.grid_line_color = None
+  plt.ygrid.grid_line_color = None
+  plt.axis.visible = False
+  plt.patches('xs','ys', 
+              source = geosourceJson, 
+              fill_color = {'field' : colorMode, 
+                            'transform' : colorMapper},
+              line_color = 'purple', 
+              line_width = 0.5, 
+              fill_alpha = 0.85)
+  plt.add_layout(colorBar, 'right')
+  plt.add_tools(hoverTool)
+  if enableTapTool:
+    plt.add_tools(TapTool())
+  if enableToolbar:
+    plt.toolbar.autohide = True
+  if plt.title is not None:
+    plt.title.text_font_size = '30pt'
+  return plt
+
 def covid19_plot(covid19_geosource, 
                  input_df=None,
                  input_field=None,
@@ -58,39 +136,22 @@ def covid19_plot(covid19_geosource,
                  map_overlay=True,
                  palette_type='OrRd',
                  custom_hovertool=True,
+                 enable_advancedStats=False,
                  enable_toolbar=False):
-  if palette_type.lower()=='OrRd'.lower():
-    palette = OrRd[9]
-    palette = palette[::-1]
-  else:
-    palette = brewer['Oranges']
-    palette = palette[::-1]
   
-  if custom_hovertool:
-    hover  = HoverTool(tooltips ="""<strong><font face="Arial" size="3">@state</font></strong> <br>
-                                    <font face="Arial" size="3">Cases: @total_cases</font><br>
-                                    <font face="Arial" size="3">Deaths: @deaths </font>""")
-  else:
-    hover = HoverTool(tooltips = [('State','@state'),
-                                  ('Cases', '@total_cases'),
-                                  #('Discharged/migrated', '@discharged'),
-                                  ('Deaths', '@deaths')])
-  
-  color_mapper = LinearColorMapper(palette = palette, 
-                                   low = 0, 
-                                   high = int(10*(np.ceil(np.max(input_df[input_field].values)/10))))
-  format_tick = NumeralTickFormatter(format=str(input_df[input_field].values))
+  palette = CustomPalette(palette_type)
+  color_mapper = LinearColorMapper(palette=palette, 
+                                   low=0, 
+                                   high=int(10*(np.ceil(np.max(input_df[input_field].values)/10))))
+  format_tick = NumeralTickFormatter(format=str(input_df[input_field].values.astype('int')))
   color_bar = ColorBar(color_mapper=color_mapper, 
-                       label_standoff=10, 
+                       label_standoff=11, 
                        formatter=format_tick,
                        border_line_color=None, 
                        major_label_text_font_size='16px',
                        location = (0, 0))
-  if map_overlay:
-    xmin = 8450000
-    xmax = 10000000
-    ymin = 850000
-    ymax = 4550000
+  xmin, xmax, ymin, ymax = MapOverlayFormatter(map_overlay)
+  hover = CustomHoverTool(enable_advancedStats, custom_hovertool)
 
   plt = figure(title = plot_title,
                x_range=(xmin, xmax) if map_overlay else None,
@@ -98,40 +159,58 @@ def covid19_plot(covid19_geosource,
                tools='save' if enable_toolbar else '', 
                plot_height = 640, plot_width = 640,
                toolbar_location = 'left' if enable_toolbar else None,
-               #lod_factor=int(1e7),
-               #lod_threshold=int(2),
+               lod_factor=int(1e7),
+               lod_threshold=int(2),
                output_backend="webgl")
   
-  if map_overlay:
-    wmts = WMTSTileSource(url="http://c.tile.openstreetmap.org/{Z}/{X}/{Y}.png")
-    plt.add_tile(wmts)
-    plt.xaxis.axis_label = 'longitude'
-    plt.yaxis.axis_label = 'latitude'
-    
-  plt.xgrid.grid_line_color = None
-  plt.ygrid.grid_line_color = None
-  plt.axis.visible = False
-  plt.patches('xs','ys', 
-              source = covid19_geosource, 
-              fill_color = {'field' : 'deaths', 
-                            'transform' : color_mapper},
-              line_color = 'purple', 
-              line_width = 0.5, 
-              fill_alpha = 0.85)
-  plt.add_layout(color_bar, 'right')
-  plt.add_tools(hover)
-  if plot_title is not None:
-    plt.title.text_font_size = '34pt'
+  plt = geographic_overlay(plt, 
+                           geosourceJson=covid19_geosource,
+                           colorBar=color_bar,
+                           colorMapper=color_mapper,
+                           colorMode=input_field,
+                           hoverTool=hover,
+                           mapOverlay=map_overlay,
+                           enableToolbar=enable_toolbar,
+                           enableTapTool=True if enable_advancedStats else False)
+  
   return plt
 
+preds_df=pd.read_csv('https://github.com/MoadComputer/covid19-visualization/raw/master/data/Coronavirus_stats/India/experimental/output_preds.csv')
+
+preds_df.columns=['id', 'state', 'preds_cases']
+preds_df.astype({'preds_cases': 'int'})
+preds_covid19_df=pd.merge(preds_df, covid19_data, on='state', how='left')
+preds_covid19_df=preds_covid19_df.fillna(0)
+preds_covid19_df['preds_cases']=preds_covid19_df['preds_cases'].astype('int')
+preds_covid19_df['deaths']=preds_covid19_df['deaths'].astype('int')
+del preds_covid19_df['ID']
+del preds_covid19_df['id']
+del preds_covid19_df['discharged']
+
+merged_preds_data = covid19_json(preds_covid19_df, India_statewise)
+merged_preds_json = merged_preds_data['json_data']
+preds_covid19_data=merged_preds_data['data_frame']
+
+preds_covid19_geosource=GeoJSONDataSource(geojson=merged_preds_json)
 covid19_geosource=GeoJSONDataSource(geojson=merged_json)
 
 plot_title=None#'COVID19 outbreak in India'
 app_title='COVID19 India'
-_covid19_plot = covid19_plot(covid19_geosource, 
-                             input_df=covid19_data,
-                             input_field='deaths',
-                             plot_title=plot_title)
+
+basic_covid19_plot = covid19_plot(covid19_geosource, 
+                                  input_df=covid19_data,
+                                  input_field='deaths',
+                                  plot_title=plot_title)
+
+advanced_covid19_plot = covid19_plot(preds_covid19_geosource, 
+                                     input_df=preds_covid19_data,
+                                     input_field='preds_cases',
+                                     enable_advancedStats=True,
+                                     plot_title=None)
+
 curdoc().title=app_title
-covid19_layout = column(_covid19_plot)
+basicPlot_tab = Panel(child=basic_covid19_plot, title=" ■■■ ")
+advancedPlot_tab = Panel(child=advanced_covid19_plot, title="Advanced")
+covid19_tabs = Tabs(tabs=[basicPlot_tab, advancedPlot_tab])
+covid19_layout = covid19_tabs#column(basic_covid19_plot)
 curdoc().add_root(covid19_layout)
