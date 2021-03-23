7f62506b1289193132b910e758de78257046ffef
Rahul Remanan <rahul@remanan.net>
2021-03-23 11:19:03 +0530

    Data for 23rd March,2021

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -28,7 +28,7 @@ from bokeh.models import GeoJSONDataSource,LinearColorMapper,ColorBar,NumeralTic
 
 verbose=False
 enable_GeoJSON_saving=False
-LAST_UPDATE_DATE='22-March-2021'
+LAST_UPDATE_DATE='23-March-2021'
 
 def apply_corrections(input_df):
   for state in list(input_df['state'].values):
