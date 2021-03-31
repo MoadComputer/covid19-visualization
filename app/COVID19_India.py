80b21ae715e558042a9a4a861d94f515b515e2f2
Rahul Remanan <rahul@remanan.net>
2021-03-31 16:50:32 +0530

    Fixing plot width issues

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -600,7 +600,7 @@ def model_performancePlot(source,
                                              ('Cases: ','@y_cases')]
 
     perfPlot=figure(#y_axis_type="log",y_range=(2.5e4,7.5e4), 
-                    plot_height=530, plot_width=530,
+                    plot_height=510, plot_width=510,
                     tools='hover', 
                     toolbar_location=None,
                     tooltips=TOOLTIPS)
