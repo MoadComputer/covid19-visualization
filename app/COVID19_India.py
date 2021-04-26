48f3c6a758a63765530759e5ec9e12999ec67dc3
Rahul Remanan <rahul@remanan.net>
2021-04-26 13:24:18 +0530

    Minor stylisctic changes to Bokeh tool ...

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -438,7 +438,7 @@ basic_covid19_plot = covid19_plot(covid19_geosource,
                                   enable_IndiaStats=True,
                                   integer_plot=True,
                                   plot_title=plot_title)
-basicPlot_tab = Panel(child=basic_covid19_plot, title=" ■■■ ")
+basicPlot_tab = Panel(child=basic_covid19_plot, title=" 🏠 ")
 
 if advanced_mode:
   preds_df.columns=['id','state',                                                   \
