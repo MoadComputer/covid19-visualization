9a4d6d661909ce714c7496fb610fde85c7665080
Rahul Remanan <rahul@remanan.net>
2021-06-22 19:26:30 +0530

    Forecasts for 22nd June,2021

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -30,7 +30,7 @@ verbose=False
 enable_GeoJSON_saving=False
 
 DATA_UPDATE_DATE='22-June-2021'
-FORECASTS_UPDATE_DATE='21-June-2021'
+FORECASTS_UPDATE_DATE='22-June-2021'
 
 def apply_corrections(input_df):
   for state in list(input_df['state'].values):
