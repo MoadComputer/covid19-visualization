705f674537d73e0b66b40f6f0f493eac2da46cb8
Rahul Remanan <rahul@remanan.net>
2021-11-14 12:17:36 +0530

    Data for 14th November,2021

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -19,7 +19,7 @@ from bokeh.models import GeoJSONDataSource,LinearColorMapper,ColorBar,NumeralTic
 verbose=False
 enable_GeoJSON_saving=False
 
-DATA_UPDATE_DATE='13-November-2021'
+DATA_UPDATE_DATE='14-November-2021'
 FORECASTS_UPDATE_DATE='13-November-2021'
 
 def apply_corrections(input_df):
