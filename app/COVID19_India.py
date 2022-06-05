074a6d533f39eff5147b646cac7773100357cdae
Rahul Remanan <rahul@remanan.net>
2022-06-06 00:05:14 +0530

    Data for 6th June 2022

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -23,7 +23,7 @@ verbose=False
 enable_GeoJSON_saving=False
 
 DATA_UPDATE_DATE='05-June-2022'
-FORECASTS_UPDATE_DATE='04-June-2022'
+FORECASTS_UPDATE_DATE='05-June-2022'
 
 def apply_corrections(input_df):
   for state in list(input_df['state'].values):
