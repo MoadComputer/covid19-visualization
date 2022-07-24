63163fd26d600aa9e13b1042d89f6f5405f26892
Rahul Remanan <rahul@remanan.net>
2022-07-24 13:52:38 +0530

    Data for 24th July 2022

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -22,7 +22,7 @@ from bokeh.models import GeoJSONDataSource,LinearColorMapper,ColorBar,        \
 verbose=False
 enable_GeoJSON_saving=False
 
-DATA_UPDATE_DATE='23-July-2022'
+DATA_UPDATE_DATE='24-July-2022'
 FORECASTS_UPDATE_DATE='23-July-2022'
 
 def apply_corrections(input_df):
