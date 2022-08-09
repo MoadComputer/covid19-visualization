4043b1c7e1396c93daff2cb2fe3b8b38b3c1a45a
Rahul Remanan <rahul@remanan.net>
2022-08-09 22:20:47 +0530

    Forecasts for 9th August 2022

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -23,7 +23,7 @@ verbose=False
 enable_GeoJSON_saving=False
 
 DATA_UPDATE_DATE='09-August-2022'
-FORECASTS_UPDATE_DATE='08-August-2022'
+FORECASTS_UPDATE_DATE='09-August-2022'
 
 def apply_corrections(input_df):
   for state in list(input_df['state'].values):
