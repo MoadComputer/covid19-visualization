d14951d1acee8c8225cd520b63ecc5134f8c433e
Rahul Remanan <rahul@remanan.net>
2023-03-02 00:15:10 +0530

    Forecasts for 1st March 2023

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -23,7 +23,7 @@ verbose=False
 enable_GeoJSON_saving=False
 
 DATA_UPDATE_DATE='01-March-2023'
-FORECASTS_UPDATE_DATE='28-February-2023'
+FORECASTS_UPDATE_DATE='01-March-2023'
 
 def apply_corrections(input_df):
   for state in list(input_df['state'].values):
