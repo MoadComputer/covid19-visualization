4f5e8f1177ad13baa58efaade4162d4346e8c20d
Rahul Remanan <rahul@remanan.net>
2023-04-27 23:00:39 +0530

    Forecasts for 27th April 2023

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -23,7 +23,7 @@ verbose=False
 enable_GeoJSON_saving=False
 
 DATA_UPDATE_DATE='27-April-2023'
-FORECASTS_UPDATE_DATE='26-April-2023'
+FORECASTS_UPDATE_DATE='27-April-2023'
 
 def apply_corrections(input_df):
   for state in list(input_df['state'].values):
