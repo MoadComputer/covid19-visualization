24a0c01ca84cc79a28021af58371d774e2a23b72
Rahul Remanan <rahul@remanan.net>
2023-06-21 12:53:55 +0530

    Data [poor quality official source] for 21st June 2023

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -22,8 +22,8 @@ from bokeh.models import GeoJSONDataSource,LinearColorMapper,ColorBar,        \
 verbose=False
 enable_GeoJSON_saving=False
 
-DATA_UPDATE_DATE='20-June-2023'
-FORECASTS_UPDATE_DATE='20-June-2023'
+DATA_UPDATE_DATE='21-June-2023'
+FORECASTS_UPDATE_DATE='21-June-2023'
 
 def apply_corrections(input_df):
   for state in list(input_df['state'].values):
