ae847e1de86b63981b9b2e8becc6d0adbb4952b7
Rahul Remanan <rahul@remanan.net>
2023-12-08 10:52:50 +0530

    Data [poor quality official source] for 8th December 2023

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -27,7 +27,7 @@ except:
 verbose=False
 enable_GeoJSON_saving=False
 
-DATA_UPDATE_DATE='07-December-2023'
+DATA_UPDATE_DATE='08-December-2023'
 FORECASTS_UPDATE_DATE='07-December-2023'
 
 def apply_corrections(input_df):
