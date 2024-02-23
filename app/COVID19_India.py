fee19e228cc37bdb15613bf0bd5273082d5dffc0
Rahul Remanan <rahul@remanan.net>
2024-02-23 22:59:15 +0530

    Forecasts for 23rd February 2024

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -28,7 +28,7 @@ verbose=False
 enable_GeoJSON_saving=False
 
 DATA_UPDATE_DATE='23-February-2024'
-FORECASTS_UPDATE_DATE='22-February-2024'
+FORECASTS_UPDATE_DATE='23-February-2024'
 
 def apply_corrections(input_df):
   for state in list(input_df['state'].values):
