6592a519e9d2403a16b4138e14158e23ad641cc9
Rahul Remanan <rahul@remanan.net>
2024-05-17 12:53:03 +0530

    Data [poor quality official source] for 17th May 2024

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -42,7 +42,7 @@ else:
 verbose=False
 enable_GeoJSON_saving=False
 
-DATA_UPDATE_DATE='16-May-2024'
+DATA_UPDATE_DATE='17-May-2024'
 FORECASTS_UPDATE_DATE='16-May-2024'
 
 DATA_URL='https://github.com/MoadComputer/covid19-visualization/raw/main/data'
