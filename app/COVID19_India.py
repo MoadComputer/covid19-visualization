baf6958a5c1940c15231bd26a368405f765b0cb6
Rahul Remanan <rahul@remanan.net>
2025-06-05 13:16:40 +0530

    Updated SARS-CoV2 case Bokeh app for 5th June 2025 ...

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -42,7 +42,7 @@ else:
 verbose=False
 enable_GeoJSON_saving=False
 
-DATA_UPDATE_DATE='04-June-2025'
+DATA_UPDATE_DATE='05-June-2025'
 FORECASTS_UPDATE_DATE='04-June-2025'
 
 DATA_URL='https://raw.githubusercontent.com/MoadComputer/covid19-visualization/main/data'
