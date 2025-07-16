89be4647856cf7038b6187215f82892c1ed3502e
Rahul Remanan <rahul@remanan.net>
2025-07-16 14:38:30 +0530

    Updated SARS-CoV2 case statistics for July 16th, 2025 ...

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -44,7 +44,7 @@ else:
 verbose=False
 enable_GeoJSON_saving=False
 
-DATA_UPDATE_DATE='15-July-2025'
+DATA_UPDATE_DATE='16-July-2025'
 FORECASTS_UPDATE_DATE='15-July-2025'
 
 DATA_URL='https://raw.githubusercontent.com/MoadComputer/covid19-visualization/main/data'
