511e0be870bfab7ecde98f816e66c215f0479ce6
Rahul Remanan <rahul@remanan.net>
2025-08-18 10:46:45 +0530

    Updated SARS-CoV2 case statistics Bokeh app for August 18th, 2025 ...

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -44,7 +44,7 @@ else:
 verbose=False
 enable_GeoJSON_saving=False
 
-DATA_UPDATE_DATE='17-August-2025'
+DATA_UPDATE_DATE='18-August-2025'
 FORECASTS_UPDATE_DATE='17-August-2025'
 
 DATA_URL='https://raw.githubusercontent.com/MoadComputer/covid19-visualization/main/data'
