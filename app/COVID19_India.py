74c1b561e1c609e543f76eedde7986c86c401515
Rahul Remanan <rahul@remanan.net>
2025-09-01 10:45:26 +0530

    Updated SARS-CoV2 case statistics for 1st September 2025 ...

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -46,7 +46,7 @@ else:
 verbose = False
 enable_GeoJSON_saving = False
 
-DATA_UPDATE_DATE = '31-August-2025'
+DATA_UPDATE_DATE = '01-September-2025'
 FORECASTS_UPDATE_DATE = '31-August-2025'
 
 DATA_URL = 'https://raw.githubusercontent.com/MoadComputer/covid19-visualization/main/data'
