624cf7982284c6987b9a711232dda4c5dfdced07
Rahul Remanan <rahul@remanan.net>
2025-10-02 20:56:01 +0530

    Updated SARS-CoV2 case forecasts for 2nd October 2025 ...

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -49,7 +49,7 @@ verbose = False
 enable_GeoJSON_saving = False
 
 DATA_UPDATE_DATE = '02-October-2025'
-FORECASTS_UPDATE_DATE = '01-October-2025'
+FORECASTS_UPDATE_DATE = '02-October-2025'
 
 DATA_URL = 'https://raw.githubusercontent.com/MoadComputer/covid19-visualization/main/data'
 LOCAL_DATA_DIR = './GitHub/MoadComputer/covid19-visualization/data'
