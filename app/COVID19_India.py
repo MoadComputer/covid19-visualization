c75d76db28b78057b182659145b6dbb763e82554
Rahul Remanan <rahul@remanan.net>
2025-11-02 12:29:33 +0530

    Updated SARS-CoV2 case statistics for 2nd November 2025 ...

--- app/COVID19_India.py
+++ app/COVID19_India.py
@@ -48,7 +48,7 @@ else:
 verbose = False
 enable_GeoJSON_saving = False
 
-DATA_UPDATE_DATE = '01-November-2025'
+DATA_UPDATE_DATE = '02-November-2025'
 FORECASTS_UPDATE_DATE = '01-November-2025'
 
 DATA_URL = 'https://raw.githubusercontent.com/MoadComputer/covid19-visualization/main/data'
