40bb907b24eb567916bcd10a0b06b99a408a262a
Rahul Remanan <rahul@remanan.net>
2025-12-08 09:59:29 +0530

    Updated SARS-CoV2 case statistics for 8th December 2025 ...

--- app/India_SARS_CoV2.py
+++ app/India_SARS_CoV2.py
@@ -49,7 +49,7 @@ else:
 verbose = False
 enable_GeoJSON_saving = False
 
-DATA_UPDATE_DATE = '07-December-2025'
+DATA_UPDATE_DATE = '08-December-2025'
 FORECASTS_UPDATE_DATE = '07-December-2025'
 
 DATA_URL = 'https://raw.githubusercontent.com/MoadComputer/covid19-visualization/main/data'
