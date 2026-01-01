22e32bcbe3688197e918e73948deeebf7d963587
Rahul Remanan <rahul@remanan.net>
2026-01-01 11:02:26 +0530

    Updated SARS-CoV2 case statistics for 1st January 2026 ...

--- app/India_SARS_CoV2.py
+++ app/India_SARS_CoV2.py
@@ -49,8 +49,8 @@ else:
 verbose = False
 enable_GeoJSON_saving = False
 
-DATA_UPDATE_DATE = '31-December-2025'
-FORECASTS_UPDATE_DATE = '31-December-2025'
+DATA_UPDATE_DATE = '01-January-2026'
+FORECASTS_UPDATE_DATE = '31-December-2025' # '01-January-2026' #
 
 DATA_URL = 'https://raw.githubusercontent.com/MoadComputer/covid19-visualization/main/data'
 LOCAL_DATA_DIR = './GitHub/MoadComputer/covid19-visualization/data'
